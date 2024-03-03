07ec184e2b53a8e95577cda0afd88cd951aa0d0b
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-03 15:37:11 +0100

    Add onclick methods for buttons

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 
 class Counter extends React.Component {
+  handleAttack() {
+    alert("Attack clicked");
+  }
+
+  handleDefence() {
+    alert("Defend clicked");
+  }
+
   render() {
     return (
       <div className="row text-white" style={{ backgroundColor: "black" }}>
         <h1>Counter</h1>
-        <button style={{ width: "200px" }}>+1</button>
-        <button style={{ width: "200px" }}>-1</button>
+        <button onClick={this.handleAttack} style={{ width: "200px" }}>
+          +1
+        </button>
+        <button onClick={this.handleDefence} style={{ width: "200px" }}>
+          -1
+        </button>
       </div>
     );
-  } 
+  }
 }
 
 export default Counter;
