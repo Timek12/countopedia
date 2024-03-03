a5ab0b559689fc8e4fa91bf366d4f8f9b838be09
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-03-03 15:29:42 +0100

    Implement counter component

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 class Counter extends React.Component {
   render() {
-    return <div>Counter</div>;
-  }
+    return (
+      <div className="row text-white" style={{ backgroundColor: "black" }}>
+        <h1>Counter</h1>
+        <button style={{ width: "200px" }}>+1</button>
+        <button style={{ width: "200px" }}>-1</button>
+      </div>
+    );
+  } 
 }
 
 export default Counter;
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import logo from "./images/react.png";
 
 const Header = () => {
   return (
-    <div className="container row 70vh">
+    <div className="container row py-2">
       <img src={logo} alt="" className="w-10 col-2" ></img>
       <div className="h2 pt-4 text-black-50 col-10">React Course - CountOpedia</div>
     <hr></hr>
